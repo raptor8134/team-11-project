7e75bd172dac43df02d3af37edae3d54efb53d0c
raptor8134 <jamesnotley@gmail.com>
2021-03-27 03:10:17 -0700

    Added reading json from file for classes

--- main.js
+++ main.js
@@ -36,21 +36,31 @@ function addMeeting(url, name, start, end) {
 }
 
 //TODO make this into a json read function from a browser cache, after we make the input page
-var meetings = [ // for testing only
+//Feel free to add more fields as necessary, just dont forget the json part
+/*
+var meetings1 = [ // for testing only
 	{'url': "someurl", 'name': "History", 'start': "7:40", 'end': "8:50"},
 	{'url': "someurl", 'name': "English", 'start': "9:00", 'end': "10:10"},
 	{'url': "someurl", 'name': "Math", 'start': "10:20", 'end': "11:30"},
 	{'url': "someurl", 'name': "Spanish", 'start': "11:40", 'end': "12:50"},
 	{'url': "someurl", 'name': "After-school help", 'start': "1:50", 'end': "2:35"},
 ];
+*/
+
+var meetings = JSON.parse(data);
+
+//var meetings = {};
+//for (var i = 0; i < raw_json.meetings.length; i++) {
+//	meetings[i] = raw_json.meetings[i];
+//}
+//console.log(meetings);
 
 function addMeetingS(meetings) {
 	var retval;
 	for (var i = 0; i < meetings.length; i++) {
-		console.log(meetings[i]);
 		retval += addMeeting(meetings[i]['url'], meetings[i]['name'], meetings[i]['start'], meetings[i]['end']);
 	}
 	return retval;
 }
 
-document.getElementById('date').innerHTML = getDateString(dateFromDay(currentYear, currentDayNumber)) + addMeetingS(/*"https://us02web.zoom.us/meeting/register/tZwtdOiorDkrGtNu0lvirw_yLuV644lp6dZ"*/meetings) + document.getElementById('date').innerHTML;
+document.getElementById('date').innerHTML = getDateString(dateFromDay(currentYear, currentDayNumber)) + addMeetingS(meetings) + document.getElementById('date').innerHTML;
